3e305025511596748e3be565a11784d20abf83be
tianwei <liutianweidlut@gmail.com>
2013-09-04 22:39:58 +0800

    Update exportdbdata.py
    export files

--- backstage/deploy/exportdbdata.py
+++ backstage/deploy/exportdbdata.py
@@ -3,36 +3,43 @@
 import os
 import sys
 from xml.etree.ElementTree import ElementTree, Element, SubElement
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append('/home/amaris/dev-mixxx/backstage/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'backstage.settings_dev'
 
+RESOURCE_PATH = "/home/amaris/dev-mixxx/backstage/savefiles"
+
 from backstage.gui.models import FileStorage, MappingPresetObject, FileTypeDict
 
 
+
+def export_files(pid):
+    file_objecs = FileStroge.objects.filter(pid=pid)
+    for obj in file_objects:
+        fname = os.path.join(RESOUCE_PATH)
+        default_storage.save(fname, ContentFile(obj.file_obj.read()))
+        
+
 def export(path):
     root = Element('presets')
-    xmlFiles = FileStorage.objects.filter(file_type=FileTypeDict.objects.get(category='xml'))
-    for f in xmlFiles:
-        pid = f.mapping_preset_id.pid
-        print pid
+    xmlFiles = FileStorage.objects.filter(file_type.category='xml')
+    for xml in xmlFiles:
+        pid = xml.mapping_preset_id.pid
         try:
             status = MappingPresetObject.objects.get(pid=pid).preset_status.category
-        except Exception:
+        except Exception,err:
+            print err
             print "this pid is not exists in the database"
         else:
-            print status
             preset = SubElement(root, 'preset')
             preset.set('filename', f.file_name)
             preset.set('pid', pid)
             preset.set('status', status)
-            #fname = SubElement(preset, 'filename')
-            #fname.text = f.file_name
-            #presetID = SubElement(preset, 'pid')
-            #presetID.text = pid
-            #presetStatus = SubElement(preset, 'status')
-            #presetStatus.text = status
+            export_files(pid)
+            
     tree = ElementTree(root)
     tree.write(os.path.join(path, 'mapScript.xml'), encoding='utf-8')
 
